e2b21cb140b7617903a984a2a053321aed5432a5
dskulkarni2006@gmail.com <dskulkarni2006@gmail.com>
2026-07-11 14:53:20 +0530

    Section-2 code work shader animations

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,17 @@ function App() {
   return (
     <>
       <main>
-        <Canvas
+        <div className="images">
+          <img id="tommorowland" src="/tommorowland.png" alt="" />
+          <img id="navy-pier" src="/navy-pier.png" alt="" />
+          <img id="msi-chicago" src="/msi-chicago.png" alt="" />
+          <img id="phone" src="/phone.png" alt="" />
+          <img id="kikk" src="/kikk.png" alt="" />
+          <img id="kennedy" src="/kennedy.png" alt="" />
+          <img id="opera" src="/opera.png" alt="" />
+        </div>
+        <Canvas id="tommorowland"
+          id="canvas-elem"
           style={{
             height: "100vh",
             width: "100vw",
@@ -14,9 +24,6 @@ function App() {
             top: 0,
             left: 0,
             zIndex: 1,
-            backgroundImage: "url(/background-l-2.png)",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
           }}
         >
           <Dog />
@@ -73,7 +80,38 @@ function App() {
           <div className="first-line"></div>
           <div className="second-line"></div>
         </section>
-        <section id="section-2"></section>
+        <section id="section-2">
+          <div className="titles">
+            <div img-title="tommorowland" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>Tomorrowland</h1>
+            </div>
+            <div img-title="navy-pier" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>Navy Pier</h1>
+            </div>
+            <div img-title="msi-chicago" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>MSI Chicago</h1>
+            </div>
+            <div img-title="phone" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>This was Louie's Phone</h1>
+            </div>
+            <div img-title="kikk" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>KIKK Festival 2018</h1>
+            </div>
+            <div img-title="kennedy" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>The Kennedy Center</h1>
+            </div>
+            <div img-title="opera" className="title">
+              <small>2020 - ONGOING</small>
+              <h1>Royal Opera Of Wallonia</h1>
+            </div>
+          </div>
+        </section>
         <section id="section-3"></section>
       </main>
     </>
